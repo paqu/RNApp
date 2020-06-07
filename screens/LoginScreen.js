494a73247c775db58ee556e8ec61f71adcd882d5
paqu <pawel.kusz1993@gmail.com>
2020-06-07 16:46:09 +0200

    Handle keyboard actions

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -33,49 +33,47 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View
-          style={{
-            marginTop: 60,
-            marginBottom: 80,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Image source={require("../assets/logo.png")} />
-          <Text
-            style={[
-              styles.text,
-              { marginTop: 10, fontSize: 22, fontWeight: "500" },
-            ]}
-          >
-            myBudget
-          </Text>
-        </View>
-
-        <FormTextInput title={strings.LOGIN_TITLE} />
-        <FormTextInput title={strings.PASSWORD} secureTextEntry={true} />
-
-        <Text style={[styles.text, styles.link, { textAlign: "right" }]}>
-          Forgot Password?
-        </Text>
-
-        <FormButton label={strings.LOGIN} onPress={this.handleLoginPress} />
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Image source={require("../assets/logo.png")} style={styles.logo} />
         <Text
           style={[
             styles.text,
             {
-              fontSize: 14,
-              color: colors.GRAY,
-              textAlign: "center",
-              marginTop: 24,
+              marginTop: 10,
+              marginBottom: 20,
+              fontSize: 22,
+              fontWeight: "500",
             },
           ]}
         >
-          Don't have an account?{" "}
-          <Text style={[styles.text, styles.link]}>Register Now</Text>
+          myBudget
         </Text>
-      </View>
+
+        <View style={styles.form}>
+          <FormTextInput title={strings.LOGIN_TITLE} />
+          <FormTextInput title={strings.PASSWORD} secureTextEntry={true} />
+
+          <Text style={[styles.text, styles.link, { textAlign: "right" }]}>
+            Forgot Password?
+          </Text>
+
+          <FormButton label={strings.LOGIN} onPress={this.handleLoginPress} />
+          <Text
+            style={[
+              styles.text,
+              {
+                fontSize: 14,
+                color: colors.GRAY,
+                textAlign: "center",
+                marginTop: 24,
+              },
+            ]}
+          >
+            Don't have an account?{" "}
+            <Text style={[styles.text, styles.link]}>Register Now</Text>
+          </Text>
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -84,7 +82,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.WHITE,
-    paddingHorizontal: 30,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  logo: {
+    flex: 1,
+    width: "100%",
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 30,
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    width: "80%",
   },
   text: {
     fontFamily: "Avenir Next",
